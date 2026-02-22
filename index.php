6c41f9d22b6ae7847192b3e19b60e6f1a57036fa
Ankita Singh <ankitasingh9918@gmail.com>
2026-02-22 14:13:09 +0530

    Restore favicon link in the header for proper icon display

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tulsi Inter College</title>
-    <!-- <link rel="shortcut icon" href="./assets/images/footer.jpeg" type="image/x-icon"> -->
+    <link rel="shortcut icon" href="./assets/images/footer.jpeg" type="image/x-icon">
 </head>
 <body>
         <!-- header  -->
